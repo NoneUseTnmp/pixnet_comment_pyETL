2757faa6b8c6a0be7b2907665e1d7a88917ac879
johnson18527 <84448990+johnson18527@users.noreply.github.com>
2021-07-18 15:38:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,52 +5,74 @@ import os
 import pandas as pd
 import time
 import random
+from fake_useragent import UserAgent as ua
 
 columns=['名稱', '內容', '推','發文時間','店家名稱','作者給分','平均分數','評分人數']
 data=[]
+none_page=0
+mis_pag=[]
+#st=[]
+thrt=0
 
 keywords = ['台北美食']
 for keyword in keywords:
-    print(f'---------{keyword}職位---------')
+    print(f'---------{keyword}---------')
     
-    for i in range(2):
+    for i in range(20):
         r=i+1
         url='https://www.pixnet.net/mainpage/api/tags/%E5%8F%B0%E5%8C%97%E7%BE%8E%E9%A3%9F/feeds?page={}&per_page=10&filter=comments&sort=latest&refer=https%3A%2F%2Fwww.pixnet.net%2Fblog%2Farticles%2Fcategory%2F26'.format(r)
-        
+        ua1=ua().random
         params={'page':r,'per_page':'10','filter':'comments','sort':'latest','refer':'https://www.pixnet.net/blog/articles/category/26'}
-        headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'}
+        headers = {'User-Agent':"".format(ua1)}
         ss=requests.session()
         try:
             res =ss.get(url=url , headers=headers , params=params)
         except IndexError as e:
             break
         js=json.loads(res.text)
-        for n in range(0,9):
-            name=js['data']['feeds'][n]['display_name']
-            con=js['data']['feeds'][n]['description']
-            push=js['data']['feeds'][n]['total_push']
-            t = time.localtime(int(js['data']['feeds'][n]['created_at']))
-            a=time.asctime(t)
-            poi=js['data']['feeds'][n]['poi']
-            if len(poi)!=0:
-                sn=js['data']['feeds'][n]['poi']['name']
-                memcerr=js['data']['feeds'][n]['poi']['member_rating']
-                avgr=js['data']['feeds'][n]['poi']['rating']['avg']
-                countr=js['data']['feeds'][n]['poi']['rating']['count']
-            else:
-                sn=None
-                memcerr= None
-                avgr=None
-                countr=None
-       
-                
-      
+        if len(js['data']['feeds']) !=0:
             
-            rdata=[name,con,push,a,sn,memcerr,avgr,countr]
-            data.append(rdata)
+            
+            for n in range(0,10):
+                name=js['data']['feeds'][n]['display_name']
+                con=js['data']['feeds'][n]['description']
+                push=js['data']['feeds'][n]['total_push']
+                t = time.localtime(int(js['data']['feeds'][n]['created_at']))
+                a=time.asctime(t)
+                poi=js['data']['feeds'][n]['poi']
+                if len(poi)!=0:
+                    sn=js['data']['feeds'][n]['poi']['name']
+                    memcerr=js['data']['feeds'][n]['poi']['member_rating']
+                    avgr=js['data']['feeds'][n]['poi']['rating']['avg']
+                    countr=js['data']['feeds'][n]['poi']['rating']['count']
+                else:
+                    sn=None
+                    memcerr= None
+                    avgr=None
+                    countr=None
+
+
+
+
+
+                rdata=[name,con,push,a,sn,memcerr,avgr,countr]
+                data.append(rdata)
+        else:
+            none_page+=1
+            mis_pag.append(js['data']['page'])
         
-        time.sleep(random.randint(1,10))
+        rtt=random.randint(1,5)
+        time.sleep(rtt)
+        print("page{} break {} s".format(r,rtt))
         
+        thrt=thrt+rtt
+        
+        
+print("none_page:{}".format(none_page))
+print('total_break_time: {}s'.format(thrt))
+print('total_page: {}'.format(r))
+#print("each_break_time:{}".format(st))
+print("missing_page:{}".format(mis_pag))
 df =pd.DataFrame(data=data,columns=columns)
 df          
      
